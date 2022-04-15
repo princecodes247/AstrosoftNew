bf8cb6245b39e55eb3a51cf2eecf509aa027ba39
Prince Codes <princecodes247@gmail.com>
2022-04-15 15:24:28 -0400

    refactor

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -1,30 +1,21 @@
-
-
-  history.scrollRestoration = 'manual';
-
+history.scrollRestoration = 'manual';
 
  const enterAnimation = container => {
-   let entertl = gsap.timeline({defaults: {ease: Power4.easeInOut}});
+   let enterTl = gsap.timeline({defaults: {ease: Power4.easeInOut}});
 
 
-   entertl
-   .set("section > :not(svg)", {duration: 1, y: "10px", opacity: 0})
+   enterTl
+   .set("section > :not(svg)", {y: "10px", opacity: 0})
    .to("section > :not(svg)", {duration: 1, y: "0px", opacity: 1})
  }
 
  const pageTransition = container => {
    let tl = gsap.timeline({defaults: {ease: Power4.easeInOut}});
-
-
    tl
    .to("section > :not(svg)", {duration: 1, y: "10px", opacity: 0})
-  //  .to("#loading_cover__back", {duration: 1.3, x: "100vw"}, "-=0.9")
-  //  .to("#loading_cover__mid", {duration: 1.3, x: "100vw"}, "-=1")
-  //  .to("#loading_cover__front", {duration: 1.3, x: "100vw"}, "-=1")
     .to(".loading_cover", {duration: 1.2, x: "100vw", stagger: 0.1}, "-=0.7")
    .to(".loading_cover", {duration: 0.7, x: "200vw", delay: .5, stagger: -0.1})
    .set(".loading_cover", {x: "-100vw"});
-   
  }
 
  function delay (n) {
